8f69bb8d22fc4758ea542071a8c80c50d537148e
rafiqul.islam <rafiqulislam.mca18.du@gmail.com>
2022-11-12 03:05:06 +0530

    add  two files

---
+++ 394.decode-string.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=394 lang=python3
+#
+# [394] Decode String
+#
+# algorithms
+
+#[k[k[k[]]]]
+#k[k[]k[]k[]]
+
+# @lc code=start
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack=[]
+        for i in  range(len(s)):
+            if s[i]!=']':
+                stack.append(s[i])
+            else:
+                substring=""
+                while(stack[-1]!='['):
+                    substring=stack.pop()+substring
+                stack.pop()
+                k=''
+                while stack and stack[-1].isdigit():
+                    k=stack.pop()+k
+                
+                stack.append(int(k)*substring) 
+                
+                
+        return "".join(stack)                   
+                    
+                    
+                    
+                    
+        
+# @lc code=end
+
