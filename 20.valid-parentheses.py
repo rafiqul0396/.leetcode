28a2fae41a66d4d7344bc7c8128024c6d9432028
rafiqul.islam <rafiqulislam.mca18.du@gmail.com>
2022-11-12 01:23:25 +0530

    this my first commit

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        hashmap={")":"(","}":"{","]":"["}
+        for c in s:
+            if c in hashmap:
+                if stack and stack[-1]==hashmap[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+        
+# @lc code=end
+
